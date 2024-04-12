06caf66686abe4888b7c371e4505f5a143d993b8
isadoraTais <132675651+isadoraTais@users.noreply.github.com>
2024-04-12 12:05:12 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+const contador = document.querySelectorAll(".contador");
+const tempoObjetivo01 = new Date ("2024-30-08T00:00:00")
 for(let i= 0; i< botoes.length; i++){
     botoes[i].onclick = function(){
         for(j=0; j<botoes.length; j++){
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
 }
